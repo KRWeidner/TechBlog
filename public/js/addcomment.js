9792b05661d221118c0a655222eba99c093d9f27
KatyW <ktabe13@gmail.com>
2023-08-28 22:40:23 -0500

    adding logic to add comments

---
+++ public/js/addcomment.js
@@ -0,0 +1,29 @@
+const postData = document.querySelectorAll('.form-group');
+
+const newPostFormHandler = async (event) => {
+    event.preventDefault();
+    console.log("here2");
+    // Collect values from the new comment form
+    const content = document.querySelector('#content-new-comment').value.trim();
+    const postId = postData[0].id.split("-")[1];
+  
+    if (content && postId) {
+      // Send a POST request to the API endpoint
+      console.log(content);
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({ comment_text: content, blog_id: Number(postId)}),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/post/' + postId);
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
+  document
+    .querySelector('#createComment')
+    .addEventListener('click', newPostFormHandler);
